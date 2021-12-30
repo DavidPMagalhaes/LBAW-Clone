42296ef220f483ce024c01fcc4805aee6c8bc696
Frederico Lopes <frederico.m.lps@gmail.com>
2021-12-30 20:57:28 +0000

    search almost done

--- app/Http/Controllers/SearchBarController.php
+++ app/Http/Controllers/SearchBarController.php
@@ -22,11 +22,29 @@ class SearchBarController extends Controller
     {
         $search = $request->input('search');
 
-        //se bookContent for collection acho que nao vai funcionar
+        //bookContents with requested titles 
         $bookContent = BookContent::where('title', 'LIKE', '%'.$search.'%')->get();
-        dd($bookContent->get('bookcontentid')[0]->bookcontentid);
-        $contentids= $bookContent->get('bookcontentid')[0]->bookcontentid;
+        //dd($bookContent);
+       
+        //ids
+        $contentids = $bookContent[0]->bookid;
+        //dd($contentids);
+
+
         $books = BookProduct::where('bookcontentid', '=', $contentids)->get();
+
+        dd($bookContent);
+        $counter = 0;
+        foreach($bookContent as $bookcontent){   
+            if ($counter == 0) continue;
+
+            //dd($bookcontent[$counter]);
+            if(!$bookcontent[$counter]->bookid) continue;
+            $contentids = $bookcontent[$counter]->bookid;
+            $books->add(BookProduct::where('bookcontentid', '=', $contentids)->get());
+            $counter++;
+        }
+        //dd($books);
         //$book = BookProduct::find($id);
 
         return view('pages.search', ['books' => $books]);
