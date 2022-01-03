8edb8c6b8a47f43ba0c35878592d9afdfbe85bef
84monica <up201905753@fe.up.pt>
2022-01-03 16:16:40 +0000

    Minor Changes

--- resources/views/review/reviews.blade.php
+++ resources/views/review/reviews.blade.php
@@ -12,6 +12,20 @@
     <p> Rating: {{ $review->rating }} </p>
     <p> Time posted: {{ $review->timeposted }} </p>
     <p> By User: {{ $review->getUser($review->userid)->name }} </p>
+    @if (Auth::check() && $review->userid === Auth::id())
+    <div id = "remove">
+      <form action="/user/{{ $review->userid }}/review-history/{{ $review->reviewid }}/delete" method="POST">
+        @method('delete')
+        @csrf
+        <div>
+          <button type="submit" class="red-button">Remove</button>
+        </div>
+      </form>
+    </div>	
+
+    <a href="/user/{{ $review->userid }}/review-history/{{ $review->reviewid }}/edit" class="button">Editar</a>
+
+    @endif
     <p> ------------------------------------------------------- </p>
   </div>
 @endforeach
