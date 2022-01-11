d2b4af5b0e91f7dd4e760469c59b5796668eb748
84monica <up201905753@fe.up.pt>
2022-01-11 10:28:45 +0000

    Add Payment Method

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,7 +8,7 @@
     <div class="btn-group">
         <a class="button"  href="/user/{{$user->id}}"> User Profile</a> 
         <a class="button"  href="/user/{{$user->id}}/edit"> Edit profile</a> 
-        <a class="button"  href=" {{ $user ?? ''->id }}/payment-methods"> Payment methods</a> 
+        <a class="button"  href=" {{ $user->id }}/payment-methods"> Payment methods</a> 
         <a class="button" href=" {{ $user->id }}/review-history"> Review history </a> 
         <a class="button" href=" /user/{{ $user->id }}/purchase-history"> Order history </a> 
         @if (Auth::user()->isadmin == 'True')
