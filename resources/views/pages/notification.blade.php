d8f20a74e4092e4b47845fe514ca7f8678324955
84monica <up201905753@fe.up.pt>
2022-01-17 18:34:14 +0000

    Add Notifications to DropDown Menu

--- resources/views/pages/notification.blade.php
+++ resources/views/pages/notification.blade.php
@@ -9,9 +9,12 @@
     @if ($notification->orderid != null)
     <p> {{ $notification->notificationmessage }} on ordered Book "{{ $notification->getBookContent($notification->getBookProduct($notification->bookid)->bookcontentid)->title }}"</p>
     <p> Time: {{ $notification->notificationtime }} </p>
+    <a class="btn btn-primary"href=" /user/{{ $notification->userid }}/purchase-history"> View Order </a> 
+
     @else
     <p> {{ $notification->notificationmessage }} </p>
     <p> Time: {{ $notification->notificationtime }} </p>
+    <a class="btn btn-primary" href=" /user/{{ $notification->userid }}/payment-methods"> View Payment Methods </a> 
     @endif
     <p> ------------------------------------------------------- </p>
   </div>
