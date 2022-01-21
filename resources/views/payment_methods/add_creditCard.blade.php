0c442d088afb016c724927f83e9f4d0f05e7977c
MartaMariz <up201907020@fe.up.pt>
2022-01-21 20:30:44 +0000

    final?

--- resources/views/payment_methods/add_creditCard.blade.php
+++ resources/views/payment_methods/add_creditCard.blade.php
@@ -11,7 +11,7 @@
         <label for="ownername">Owner Name:</label><br>
         <input type="text"  name="ownername"><br>
         <label for="cardnumber">Card Number:</label><br>
-        <input type="text"  name="cardnumber">
+        <input type="text"  name="cardnumber"><br>
         <label for="securitycode">Security Code:</label><br>
         <input type="text"  name="securitycode">
         <button type="submit" class="btn btn-primary" >Add</button>
